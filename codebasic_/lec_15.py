774493bcb8e729b521dc64c8b7b6f5ef306ea240
Sufyan Ahmad <93461561+Sufya12@users.noreply.github.com>
2023-02-27 22:08:48 +0500

    Add files via upload

---
+++ codebasic_/lec_15.py
@@ -0,0 +1,13 @@
+#Import The Math Library
+import math
+
+#Create The Function
+def calc_area(r):
+    return math.pi * r**2
+
+#Apply The If Condition
+if __name__ == '__main__':
+    #Assign The Variable Call The Function
+    area = calc_area(5)
+    #Print The Variable
+    print(area)
