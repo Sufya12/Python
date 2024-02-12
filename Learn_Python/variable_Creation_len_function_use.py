9257f397f31f5e556200886c0e2362c5a87741f4
Sufyan Ahmad <93461561+Sufya12@users.noreply.github.com>
2024-02-12 21:12:51 +0500

    Add files via upload

---
+++ Learn_Python/variable_Creation_len_function_use.py
@@ -0,0 +1,6 @@
+#Create The  Variable and use input function
+name = input("What is your's Name : ")
+#Create The  Variable and use len function
+length = len(name)
+#Print The Variable Data
+print(length)
