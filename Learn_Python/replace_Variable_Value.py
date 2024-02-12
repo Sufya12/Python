682a3543fcd45a265295860bfc3a736635df2a8f
Sufyan Ahmad <93461561+Sufya12@users.noreply.github.com>
2024-02-12 22:14:27 +0500

    Add files via upload

---
+++ Learn_Python/replace_Variable_Value.py
@@ -0,0 +1,10 @@
+#Create The  Variable and use input function
+a= input("a:")
+b= input("b:")
+#Create The  Variable and Store the value
+c = a 
+a = b
+b = c
+# #Print The Variable Data
+print("a :",a)
+print("b : ", b)
