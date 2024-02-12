dafdcb0a63673f280f526a5a41a4353bd990f846
Sufyan Ahmad <93461561+Sufya12@users.noreply.github.com>
2024-02-12 22:12:57 +0500

    Add files via upload

---
+++ Learn_Python/band_Name_Generator.py
@@ -0,0 +1,9 @@
+#Create a greeting for your program
+print("Welcome to Band Name Generator.")
+# Ask the User for the city that they grew up in.
+city_name = input("What's name of the city you grew up in?\n")
+#Ask the User for the name of a pet 
+pet_name = input("What's your pet's name?\n")
+#Combine the name of their city and pet and show them their band name 
+print("Your band name could be "+ city_name+" "+pet_name)
+
