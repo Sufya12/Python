a070801708e6a0386941ddf242a6d25e28610253
Sufyan Ahmad <93461561+Sufya12@users.noreply.github.com>
2024-02-12 19:27:52 +0500

    Add files via upload

---
+++ Learn_Python/Fix_the_error.py
@@ -0,0 +1,11 @@
+#Fix the Error
+""" print(Day 1 - String Manipulation")
+print ("String Concatenation is done with the "+"sign")
+   print('e.g print("Hello"+"World")')
+print(("New Lines can be created within a backlash and n.")
+      """
+#Fix the code
+print("Day 1 - String Manipulation")
+print ("String Concatenation is done with the "+"sign")
+print('e.g print("Hello"+"World")')
+print("New Lines can be created within a backlash and n.")
